5d2302f3c2c4b089f9106784cb774ceb59ac8825
Sierra <Parkersierra40@gmail.com>
2026-08-16 22:01:16 -0400

    about page added

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './about.html',
-  styleUrl: './about.css',
+  styleUrls: ['./about.css'],
 })
-export class About {}
+export class About {
+
+  features = [
+    {
+      title: 'Premium Beans',
+      description: 'We carefully select quality coffee beans to create rich and flavorful drinks.'
+    },
+    {
+      title: 'Fresh Pastries',
+      description: 'Our pastries are made fresh to pair perfectly with your favorite coffee.'
+    },
+    {
+      title: 'Cozy Atmosphere',
+      description: 'Brew Haven is a comfortable neighborhood space where you can relax, work, or connect.'
+    }
+  ];
+
+}
